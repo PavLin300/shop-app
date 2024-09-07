4a9c581978588142336590495dadf2ac5e28c088
Pasha <p.buriak2005@gmail.com>
2024-09-07 21:15:16 +0300

    delete trash

--- colorListWContext/color-hooks.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react";
-
-const ColorContext = createContext(); // создаем контекст
-
-const useColors = ()=>useContext(ColorContext); // создаем кастомный хук, который возвращает объект с контекста
-
-export {ColorContext, useColors}
